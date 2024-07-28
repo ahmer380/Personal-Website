e5eea3510a8acf14d397836be077ccd0821c4af7
Ahmer Alam (k22008566) <ahmer.alam@kcl.ac.uk>
2024-07-28 22:35:57 +0100

    Attempt to deploy to custom url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Personal-Website/",
+  base: "/",
   build: {
     outDir: "build",
   },
